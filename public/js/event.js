9b29b7630c87225d80573ad8a64597be3f4f7272
brickoflego <googlypantsroberteng@gmail.com>
2014-11-20 22:37:51 -0800

    Ajax added and a better way to tell if connections are happening and where they are breaking.

--- public/js/event.js
+++ public/js/event.js
@@ -1,5 +1,23 @@
 $(document).ready(function(){
-	$("#registerButton").click(function() {
-		$("#registerModal").modal("toggle");
+	// $("#registerButton").click(function() {
+	// 	$("#registerModal").modal("toggle");
+	// });
+
+
+	$("#updateForm").submit(function(e) {
+		e.preventDefault(); // Stop form submission and subsequent refresh
+
+		console.log("AJAX CALL ALMOST");
+
+		$.ajax({
+			type: "POST",
+			url: "/update",
+			data: $("registerText").value,
+			success: function(data) {
+				alert(data);
+			}
+		});
+
 	});
+
 });
--- routes/index.js
+++ routes/index.js
@@ -31,33 +31,47 @@ router.post('/update', function(req, res) {
 	// }
 
 
-var connection = mysql.createConnection({
-  host     : process.env.RDS_HOSTNAME,
-  user     : process.env.RDS_USERNAME,
-  password : process.env.RDS_PASSWORD,
-  port 	   : process.env.RDS_PORT
-});
+	var connection = mysql.createConnection({
+	  host     : process.env.RDS_HOSTNAME,
+	  user     : process.env.RDS_USERNAME,
+	  password : process.env.RDS_PASSWORD,
+	  port 	   : process.env.RDS_PORT
+	});
 
+	var success = true; // flag for success. I'm sure theres a better way to do this.
 
 	connection.connect(function(err) {
 		if(err) {
+			console.log("start connection");
 			console.log(err);
-			res.render('error', {error: err, message: "Something Broke"})
+			res.end("You broke it in connect");
+			success = false;
+		} else {
+			res.end("success");
 		}
-  		//connected! (unless `err` is set)
 	});
 
-	console.log('connected as id ' + connection.threadId);
 
 	// connection.query();
 
 
 	connection.end(function(err) {
-
+		if(err) {
+			console.log("start connection in end");
+			console.log(err);
+			res.end("You broke it");
+			success = false;
+		} else {
+			res.end("success");
+		}
 	});
 
-	console.log("I could get here");
-  	// res.redirect('/');
+	// if(success) {
+	//   	res.end("success");
+
+	// } else {
+	// 	res.end("It bork");
+	// }
  	
 });
 
